cd6cf9a3f2ce31f4eb468033281cbce1797eaf17
cuanfans <cuanfans.com@gmail.com>
2026-02-02 06:15:38 +0700

    Create product.dart

---
+++ lib/domain/entities/product.dart
@@ -0,0 +1,8 @@
+class Product {
+  final int? id;
+  final String name;
+  final int price;
+  final int stock;
+
+  Product({this.id, required this.name, required this.price, required this.stock});
+}
